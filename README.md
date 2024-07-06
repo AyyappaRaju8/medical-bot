cc22336c226c3d07e977fb644aa2ce9c600eef35
Ayyappa Raju <ayyapparajuoffical@gmail.com>
2024-07-06 11:53:51 +0530

    hello

---
+++ README.md
@@ -0,0 +1,5 @@
+# coding-chatbot
+## Installation
+1. npm install
+2. create .env file and add your API key as:
+     API_KEY="AIzaSyBuzH2gQMBh2fUoc-SFmtk29C-xHMDzSZU"
---
+++ server.js
@@ -0,0 +1,85 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME, systemInstruction: "You are  a medical aasitant always answer only queries related to medicine and medical advice.", });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "You are a medic bot you provide advice few remedy always for given problem.You only answer for medical questions only."}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hello! Welcome to Medic-bot."}],
+      },
+      {
+        role: "user",
+        parts: [{ text: "Hi answer to me in less than 70 words always and dont bold your text and give next data in new line also remove * from output"}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hi there! Thanks for reaching asking let me answer that..."}],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
